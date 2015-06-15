09b1dcee21948f0a525fa3495caf00c61e6cf67f
xurz <xue571@163.com>
2015-06-16 00:04:36 +0800

    添加admin初始数据

---
+++ guu-client/doc/guu_sys_user.sql
@@ -0,0 +1,2 @@
+INSERT INTO `guu_sys_user` VALUES ('guu00001', 'ADMIN', "admin", "7BC71501FF9CCFEB580F25992E9B3DC3", 'admin@admin.com', '', '', NULL, NULL, NULL, 'admin', NULL, NULL, 1, NULL, '', '', NULL);
+
